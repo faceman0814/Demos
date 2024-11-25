0de2144868c9dd7f30e49fbda99986aefcdf79b3
faceman <feisiman@yunkra.com>
2024-11-25 09:19:11 +0800

    动态API用例提交

---
+++ Demo/WebTest/Program.cs
@@ -0,0 +1,38 @@
+using FaceMan.DynamicWebAPI.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDynamicApi(builder.Environment.WebRootPath);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseDynamicSwagger();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Privacy}/{id?}")
+//    .WithStaticAssets();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
