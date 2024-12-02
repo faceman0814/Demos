7c6a6872606bf08af6e7e298cfd4fd193a6d4c83
faceman <feisiman@yunkra.com>
2024-12-02 11:05:14 +0800

    1

--- src/FaceMan.WebTest/obj/Debug/net9.0/FaceMan.WebTest.AssemblyInfo.cs
+++ src/FaceMan.WebTest/obj/Debug/net9.0/FaceMan.WebTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FaceMan.WebTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e948dbffb468a2bee5669c49a9e2c875a28caa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eddb227de674d7a6dc68daff5c2ed89aef04ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("FaceMan.WebTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FaceMan.WebTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
